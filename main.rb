c56c436390d2eae0c9111400bf9ca083cb006522
Will Pearse <will.pearse@gmail.com>
2012-03-11 16:57:44 +0000

    Plot out graph of no. user commits

--- main.rb
+++ main.rb
@@ -24,58 +24,72 @@ File.open("tweetLog.txt") do |f|
   end
 end
 
-#Pull out SilNet's recently sent messages
-tweets = Twitter.search("to:silwoodnet")
+#Check to see if we need to do something else...
 
-#Search through for un-added 'silnet' tweets
-currentNew = 0
-tweets.each do |tweet|
-  if not currentTweets['ID'].include? tweet.id
-    currentNew += 1
-    currentTweets['text'].push tweet.text
-    text = tweet.text.sub('@silwoodnet', '')
-    if text.include? ','
-      text = text.split(',')
-      currentTweets['species'].push text[0]
-      if text[1].include? '-'
-        text = text[1].split '-'
-        currentTweets['count'].push text[0].to_i
-        currentTweets['description'].push text[1]
-      else
-       currentTweets['count'].push text[1].to_i
-       currentTweets['description'].push "NA"
-     end
-   elsif text.include? '-'
-     text = text.split('-')
-     currentTweets['species'].push text[0]
-     currentTweets['count'].push -99999
-     currentTweets['description'].push text[1]
-   else
-     currentTweets['species'].push text
-     currentTweets['count'].push -99999
-     currentTweets['description'].push "NA"
-   end
-    if tweet.geo
-      currentTweets['lat'].push tweet.geo.coordinates[0]
-      currentTweets['long'].push tweet.geo.coordinates[1]
-    else
-      currentTweets['lat'].push -9999999.9
-      currentTweets['long'].push -9999999.9
+if ARGV[0]
+  if ARGV[0] == 'userGraph'
+    require 'gchart'
+    require 'launchy'
+    users = Hash.new(0)
+    currentTweets['user'].each do |user|
+      users[user] += 1
     end
-    currentTweets['user'].push tweet.from_user
-    currentTweets['ID'].push tweet.id
-    currentTweets['timecode'].push tweet.created_at
-  end
-end
-
-#Write out new set of tweets
-if currentNew != 0
-  puts "Read #{currentNew} new tweets; total tweets in memory #{currentTweets['ID'].length}"
-  File.open("tweetLog.txt", 'w') do |f|
-    currentTweets['ID'].each_index do |i, x|
-      f.write(currentTweets['species'][i] + '^' + currentTweets['count'][i].to_s + '^' + currentTweets['lat'][i].to_s + '^' + currentTweets['long'][i].to_s + '^' + currentTweets['ID'][i].to_s + '^' + currentTweets['description'][i] + '^' + currentTweets['user'][i] + '^' + currentTweets['timecode'][i].to_s + '^' + currentTweets['text'][i] + "\n")
-    end
+    users = Hash[users.sort]
+    Launchy.open Gchart.bar(:data => users.values.sort, :axis_with_labels => 'y', :size => '600x400')
   end
 else
-  puts "No new tweets read; exiting"
+  #Pull out SilNet's recently sent messages
+  tweets = Twitter.search("to:silwoodnet")
+  #Search through for un-added 'silnet' tweets
+  currentNew = 0
+  tweets.each do |tweet|
+    if not currentTweets['ID'].include? tweet.id
+      currentNew += 1
+      currentTweets['text'].push tweet.text
+      text = tweet.text.sub('@silwoodnet', '')
+      if text.include? ','
+        text = text.split(',')
+        currentTweets['species'].push text[0]
+        if text[1].include? '-'
+          text = text[1].split '-'
+          currentTweets['count'].push text[0].to_i
+          currentTweets['description'].push text[1]
+        else
+         currentTweets['count'].push text[1].to_i
+         currentTweets['description'].push "NA"
+       end
+     elsif text.include? '-'
+       text = text.split('-')
+       currentTweets['species'].push text[0]
+       currentTweets['count'].push -99999
+       currentTweets['description'].push text[1]
+     else
+       currentTweets['species'].push text
+       currentTweets['count'].push -99999
+       currentTweets['description'].push "NA"
+     end
+      if tweet.geo
+        currentTweets['lat'].push tweet.geo.coordinates[0]
+        currentTweets['long'].push tweet.geo.coordinates[1]
+      else
+        currentTweets['lat'].push -9999999.9
+        currentTweets['long'].push -9999999.9
+      end
+      currentTweets['user'].push tweet.from_user
+      currentTweets['ID'].push tweet.id
+      currentTweets['timecode'].push tweet.created_at
+    end
+  end
+
+  #Write out new set of tweets
+  if currentNew != 0
+    puts "Read #{currentNew} new tweets; total tweets in memory #{currentTweets['ID'].length}"
+    File.open("tweetLog.txt", 'w') do |f|
+      currentTweets['ID'].each_index do |i, x|
+        f.write(currentTweets['species'][i] + '^' + currentTweets['count'][i].to_s + '^' + currentTweets['lat'][i].to_s + '^' + currentTweets['long'][i].to_s + '^' + currentTweets['ID'][i].to_s + '^' + currentTweets['description'][i] + '^' + currentTweets['user'][i] + '^' + currentTweets['timecode'][i].to_s + '^' + currentTweets['text'][i] + "\n")
+      end
+    end
+  else
+    puts "No new tweets read; exiting"
+  end
 end
